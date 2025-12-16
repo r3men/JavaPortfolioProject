a9bae25465b2aaf3d8ceceae1c4476711cf336e6
r3men <157990153+r3men@users.noreply.github.com>
2025-12-16 13:32:02 -0500

    Update AttackEntry.java

--- AttackEntry.java
+++ AttackEntry.java
@@ -3,4 +3,12 @@ public class AttackEntry {
     public String ipPrefix;
     public String ISP;
     public String timezone;
+
+    public AttackEntry(String countryName, String ipPrefix, String ISP, String timezone) {
+        this.countryName = countryName;
+        this.ipPrefix = ipPrefix;
+        this.ISP = ISP;
+        this.timezone = timezone;
+    }
 }
+
