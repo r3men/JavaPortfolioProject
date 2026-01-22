ad26eed2c2bd64d6b1f33798841f60a06162c3e0
Raymond Zhang <157990153+r3men@users.noreply.github.com>
2026-01-21 21:16:30 -0500

    Update SOC.java

--- SOC.java
+++ SOC.java
@@ -1,20 +1,32 @@
-/*
-Name: Raymond Zhang
-Teacher: Mr. Scimeca
-Class: Computer Programing II
-Date: 11/25/25
-Program Name: Geo-Tracker Simulator
-*/
+/**
+ * Name: Raymond Zhang
+ * Teacher: Mr. Scimeca
+ * Class: Computer Programing II
+ * Date: 11/25/25
+ * Program Description:
+ *    - This program is designed for those interested in learning about the career of SOC analysts,
+ *      or those who want to learn a bit of geography instead! The program utilizes several methods
+ *      for increased program readability.
+ */
 
 import java.util.*;
 
 public class SOC {
     static ArrayList<AttackEntry> attacks = new ArrayList<>();
     static String[] latestReport = null;
+
+    /**
+     * The main entry point of the program. Initializes the attack list,
+     * displays the introduction, and manages the main menu loop where
+     * the user can view instructions, start investigations, view reports,
+     * or exit the program.
+     *
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         boolean run = true;
-        ArrayList<Integer> rIDXs = new ArrayList<>();
+        ArrayList<Integer> usedIndices = new ArrayList<>();
         populateAttacks();
         intro();
         while (run) {
@@ -30,7 +42,7 @@ public class SOC {
                     displayInstructions();
                     break;
                 case 'B':
-                    latestReport = startInvestigation(attacks, rIDXs);
+                    latestReport = startInvestigation(attacks, usedIndices);
                     break;
                 case 'C':
                     if (latestReport == null) {
@@ -50,6 +62,12 @@ public class SOC {
         }
     }
 
+    /**
+     * Displays the introduction message for the Geo-Tracker Simulator.
+     * This method explains the purpose of the program and sets the scene
+     * for the user, describing how they will analyze suspicious login attempts
+     * using SOC-style investigative techniques.
+     */
     public static void intro() {
         System.out.println("Welcome to the Geo-Tracker Simulator!\n");
         System.out.println("In this program, you will analyze a series of suspicious login attempts using clues such as IP address ranges, ISPs, time zones, and user behavior patterns.\n");
@@ -58,6 +76,11 @@ public class SOC {
         System.out.println("See how well you can track down the source. Remember, your job is on the line!\n");
     }
 
+    /**
+     * Displays detailed instructions for how the investigation game works.
+     * This includes how clues are presented, how scoring works, and how
+     * the user should approach each investigation.
+     */
     public static void displayInstructions() {
         System.out.println("\n===== INSTRUCTIONS =====\n");
         System.out.println("Each investigation will provide you with a series of clues such as:");
@@ -76,24 +99,36 @@ public class SOC {
         System.out.println("\n==========================\n");
     }
 
-    public static String[] startInvestigation(ArrayList<AttackEntry> attacks, ArrayList<Integer> usedIndexes) {
+    /**
+     * Begins a new investigation by selecting a random unused attack entry.
+     * The user is presented with clues and can choose to guess the country,
+     * request hints, or give up. The method tracks score deductions based on
+     * hints used and incorrect guesses.
+     *
+     * @param attacks      the list of all possible attack entries
+     * @param usedIndices  a list of indices representing attacks already used
+     * @return a String array containing the final score, number of hints used,
+     *         number of wrong answers, correct country, and the index of the attack;
+     *         returns null if all investigations have been completed
+     */
+    public static String[] startInvestigation(ArrayList<AttackEntry> attacks, ArrayList<Integer> usedIndices) {
         Scanner s = new Scanner(System.in);
-        if (usedIndexes.size() == attacks.size()) {
+        if (usedIndices.size() == attacks.size()) {
             System.out.println("All investigations have been completed. No new cases are available.");
             return null;
         }
-        int rIDX;
+        int returnIndex;
         do {
-            rIDX = (int) (Math.random() * attacks.size());
-        } while (usedIndexes.contains(rIDX));
-        usedIndexes.add(rIDX);
+            returnIndex = (int) (Math.random() * attacks.size());
+        } while (usedIndices.contains(returnIndex));
+        usedIndices.add(returnIndex);
         boolean giveUp = false;
         boolean allHints = false;
         int score = 100;
         int hints = 0;
-        int wa = 0;
+        int wrongAnswers = 0;
         String country;
-        AttackEntry randomAttack = attacks.get(rIDX);
+        AttackEntry randomAttack = attacks.get(returnIndex);
         country = randomAttack.countryName;
         while (!giveUp) {
             System.out.println("A suspicious login was detected from an unknown country. Help us identify where it came from!");
@@ -105,22 +140,22 @@ public class SOC {
             System.out.println("1. Make a Guess");
             System.out.println("2. Receive a Hint");
             System.out.println("3. Give Up");
-            int ans = s.nextInt();
+            int userChoice = s.nextInt();
             s.nextLine();
-            if (ans == 1) {
+            if (userChoice == 1) {
                 String guess = s.nextLine();
                 if (guess.equalsIgnoreCase(randomAttack.countryName)) {
                     System.out.println("Congratulations, you correctly identified the country of origin!");
-                    score -= ((wa * 10) + (hints * 5));
+                    score -= ((wrongAnswers * 10) + (hints * 5));
                     System.out.println("For this investigation, you received a score of " + score + ". Nice job!");
                     break;
                 }
                 else {
                     System.out.println("Unfortunately that was the wrong answer. 10 points have been deducted from your total score for this investigation.");
-                    wa += 1;
+                    wrongAnswers += 1;
                 }
             }
-            else if (ans == 2) {
+            else if (userChoice == 2) {
                 if (!allHints) {
                     hints += 1;
                 }
@@ -138,7 +173,7 @@ public class SOC {
                     allHints = true;
                 }
             }
-            else if (ans == 3) {
+            else if (userChoice == 3) {
                 System.out.println("Nice try. Unfortunately you received a score of 0 for this investigation. Better luck next time!");
                 score = 0;
                 giveUp = true;
@@ -148,15 +183,24 @@ public class SOC {
             }
             System.out.println("\n");
         }
-        String[] reportInfo = new String[5];
-        reportInfo[0] = Integer.toString(score);
-        reportInfo[1] = Integer.toString(hints);
-        reportInfo[2] = Integer.toString(wa);
-        reportInfo[3] = country;
-        reportInfo[4] = Integer.toString(rIDX);
-        return reportInfo;
+        String[] investigationReport = new String[5];
+        investigationReport[0] = Integer.toString(score);
+        investigationReport[1] = Integer.toString(hints);
+        investigationReport[2] = Integer.toString(wrongAnswers);
+        investigationReport[3] = country;
+        investigationReport[4] = Integer.toString(returnIndex);
+        return investigationReport;
     }
 
+    /**
+     * Displays the results of the most recently completed investigation.
+     * This includes the correct country, the user's score, and statistics
+     * about hints and incorrect guesses.
+     *
+     * @param report a String array containing the latest investigation data:
+     *               [0] score, [1] hints used, [2] wrong answers,
+     *               [3] country, [4] attack index
+     */
     public static void viewLatestReport(String[] report) {
         System.out.println("Here is information regarding your latest report: ");
         System.out.println("Country of Origin: " + report[3]);
@@ -165,12 +209,17 @@ public class SOC {
         System.out.println("# of wrong answers submitted: " + report[2]);
     }
 
+    /**
+     * Populates the list of attack entries with predefined sample data.
+     * Each AttackEntry contains a country, IP prefix, ISP, timezone,
+     * and three progressively revealing hints used during investigations.
+     */
     public static void populateAttacks() {
         attacks.add(new AttackEntry("Germany", "192.168.1.", "Deutsche Telekom", "CET",
         "Uses umlauts in text", "Known for beer", "Famous for cars"));
-    attacks.add(new AttackEntry("Japan", "203.0.113.", "NTT Communications", "JST",
+        attacks.add(new AttackEntry("Japan", "203.0.113.", "NTT Communications", "JST",
         "Drives on the left side of the road", "Has stunning cherry blossoms", "It's very well known for its anime industry"));
-    attacks.add(new AttackEntry("Brazil", "200.0.0.", "Oi", "BRT",
+        attacks.add(new AttackEntry("Brazil", "200.0.0.", "Oi", "BRT",
         "Home to the city of Fortaleza.", "The Amazon River runs through this country.", "Prominence in soccer"));
     }
 }
